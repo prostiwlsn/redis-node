f292f4fb4cde1cf153fbf7e44a363d5f7f961cd0
prostiwlsn <prostiwlsn@gmail.com>
2024-05-31 02:14:17 +0700

    Add test data

--- test/testClient.js
+++ test/testClient.js
@@ -1,5 +1,18 @@
+const net = require('net');
+
 const client = new net.Socket();
 
+const message = 'Hello, world!';
+
 client.connect(6379, '127.0.0.1', () => {
-  client.write('PING');
+  client.write('*1\r\n$4\r\nPING\r\n');
+
+  const command = `*2\r\n$4\r\nECHO\r\n$${message.length}\r\n${message}\r\n`;
+  client.write(command);
+  
+  client.destroy();
+});
+
+client.on('data', (data) => {
+    console.log(data.toString());
 });
