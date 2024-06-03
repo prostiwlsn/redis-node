08e0e5cf3c6d41a4171f3e081998f9c7aa080a7f
prostiwlsn <prostiwlsn@gmail.com>
2024-06-04 02:19:47 +0700

    Create encoder.js

---
+++ resp/encoder.js
@@ -0,0 +1,23 @@
+const CRLF = '\r\n'
+
+export default class Encoder{
+    static encodeCommand(command){
+        const parts = command.split(" ")
+
+        let finalString = ""
+        
+        finalString += '*' + parts.length + CRLF
+
+        finalString += '$' + parts[0].length + CRLF + parts[0].toUpperCase() + CRLF
+
+        for (let i = 1; i < parts.length; i++){
+            finalString += encodeBulkString(parts[i])
+        }
+
+        return finalString
+    }
+
+    static encodeBulkString(bulkString){
+        return '$' + bulkString.length + CRLF + bulkString + CRLF
+    }
+}
