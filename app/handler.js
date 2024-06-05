c2407ae4c2fe3f97c689e77853bb7c0cb3e20b88
prostiwlsn <prostiwlsn@gmail.com>
2024-06-06 00:25:39 +0700

    Add ECHO and PING command support

---
+++ app/handler.js
@@ -0,0 +1,40 @@
+const encoder = require('../resp/encoder');
+const decoder = require('../resp/decoder');
+
+class Handler {
+    constructor() {
+        this.commands = {
+            PING: this.handlePing,
+            ECHO: this.handleEcho,
+            SET: this.handleSet,
+            GET: this.handleGet,
+            DEL: this.handleDel,
+        };
+    }
+
+    handleCommand(command, storage){
+        parsedCommand = decoder.parse(command)
+
+        return commands[parsedCommand.command]()
+    }
+
+    handlePing(args, storage){
+        return Encoder.encodeString('PONG')
+    }
+
+    handleEcho(args, storage){
+        return Encoder.encodeBulkString(command.args[0])
+    }
+
+    handleSet(args, storage){
+
+    }
+
+    handleGet(args, storage){
+
+    }
+
+    handleDel(args, storage){
+
+    }
+}
