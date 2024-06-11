800ecf0a5667a8c7a96877fea9b663153a6efda3
prostiwlsn <prostiwlsn@gmail.com>
2024-06-11 22:41:09 +0700

    Fix ttl command

--- app/handler.js
+++ app/handler.js
@@ -9,6 +9,7 @@ class Handler {
             SET: this.handleSet,
             GET: this.handleGet,
             DEL: this.handleDel,
+            TTL: this.handleTtl
         };
     }
 
@@ -30,14 +31,19 @@ class Handler {
         storage[args[0]] = {value: args[1], ttl: -1}
 
         if(args.length == 4){
-            storage[args[0]].ttl = args[3]
 
             if (args[2] == 'ex'){
+                storage[args[0]].ttl = new Date()
+                storage[args[0]].ttl.setSeconds(storage[args[0]].ttl.getSeconds() + args[3])
+
                 setTimeout(() => {
                     delete storage[args[0]];
                 }, args[3] * 1000);
             }
             else if (args[2] == 'px'){
+                storage[args[0]].ttl = new Date()
+                storage[args[0]].ttl.setMilliseconds(storage[args[0]].ttl.getMilliseconds() + args[3])
+
                 setTimeout(() => {
                     delete storage[args[0]];
                 }, args[3]);
@@ -58,6 +64,10 @@ class Handler {
         }
         delete storage[args[0]]
         return encoder.encodeString('Ok')
+    }
+
+    handleTtl(args, storage){
+        return storage[args[0]] === undefined ? encoder.encodeError("UNDEFINED", "value not found") : encoder.encodeBulkString(storage[args[0]].ttl == -1 ? (-1).toString() : (storage[args[0]].ttl.getTime() - new Date().getTime()).toString())
     }
 }
 
