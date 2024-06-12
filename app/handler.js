acec478fe8c88c4696546270a709c5e67e19a8de
prostiwlsn <prostiwlsn@gmail.com>
2024-06-12 13:51:02 +0700

    Add list commands support

--- app/handler.js
+++ app/handler.js
@@ -9,7 +9,10 @@ class Handler {
             SET: this.handleSet,
             GET: this.handleGet,
             DEL: this.handleDel,
-            TTL: this.handleTtl
+            TTL: this.handleTtl,
+            LPUSH: this.handleLpush,
+            LPOP: this.handleLpop,
+            LRANGE: this.handleLrange
         };
     }
 
@@ -55,6 +58,11 @@ class Handler {
 
     handleGet(args, storage){
         console.log(storage[args[0]])
+
+        if (storage[args[0]] != undefined && typeof storage[args[0]] != "object"){
+            return encoder.encodeError("TYPE ERROR", "value type does not match")
+        }
+
         return storage[args[0]] === undefined ? encoder.encodeError("UNDEFINED", "value not found") : encoder.encodeBulkString(storage[args[0]].value)
     }
 
@@ -62,12 +70,69 @@ class Handler {
         if(storage[args[0]] === undefined){
             return encoder.encodeError("UNDEFINED", "value not found")
         }
+        else if (typeof storage[args[0]] != "object"){
+            return encoder.encodeError("TYPE ERROR", "value type does not match")
+        }
         delete storage[args[0]]
         return encoder.encodeString('Ok')
     }
 
     handleTtl(args, storage){
         return storage[args[0]] === undefined ? encoder.encodeError("UNDEFINED", "value not found") : encoder.encodeBulkString(storage[args[0]].ttl == -1 ? (-1).toString() : (storage[args[0]].ttl.getTime() - new Date().getTime()).toString())
+    }
+
+    handleLpush(args, storage){
+        if(storage[args[0]] === undefined){
+            storage[args[0]] = []
+        }
+        else if (!Array.isArray(storage[args[0]])){
+            return encoder.encodeError("TYPE ERROR", "value type does not match")
+        }
+        
+        for(let i = 1; i < args.length; i++){
+            storage[args[0]].push(args[i])
+        }
+
+        return encoder.encodeString('Ok')
+    }
+
+    handleLpop(args, storage){
+        if(storage[args[0]] === undefined){
+            return encoder.encodeError("UNDEFINED", "value not found")
+        }
+        else if (!Array.isArray(storage[args[0]])){
+            return encoder.encodeError("TYPE ERROR", "value type does not match")
+        }
+
+        let value = storage[args[0]].pop()
+        if (storage[args[0]].length == 0){
+            delete storage[args[0]]
+        }
+
+        return encoder.encodeBulkString(value.toString())
+    }
+
+    handleLrange(args, storage){
+        if(storage[args[0]] === undefined){
+            return encoder.encodeError("UNDEFINED", "value not found")
+        }
+        else if (!Array.isArray(storage[args[0]])){
+            return encoder.encodeError("TYPE ERROR", "value type does not match")
+        }
+
+        const leftRange = parseInt(args[1])
+        let rightRange = parseInt(args[2])
+
+        if(isNaN(leftRange) || isNaN(rightRange)){
+            return encoder.encodeError("ARGUMENT ERROR", "argument type does not match")
+        }
+        else if (leftRange >= storage[args[0]].length){
+            return encoder.encodeArray([])
+        }
+
+        rightRange = rightRange >= storage[args[0]].length || rightRange < 0 ? storage[args[0]].length-1 : rightRange
+
+        return encoder.encodeArray(storage[args[0]].slice(leftRange, rightRange))
     }
 }
 
